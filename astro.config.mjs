b27b6c0bb5961b48dd8f26f768d5466298ca5ef9
Minjae Gwon <beta.underalpha@gmail.com>
2026-03-18 16:43:33 +0900

    feat: add Cloudflare support and configure Wrangler

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,32 +1,38 @@
 // @ts-check
-import { defineConfig, fontProviders } from 'astro/config';
-import react from '@astrojs/react';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig, fontProviders } from "astro/config";
+import react from "@astrojs/react";
+import tailwindcss from "@tailwindcss/vite";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
+
   vite: {
     plugins: [tailwindcss()],
   },
+
   experimental: {
     fonts: [
       {
         provider: fontProviders.google(),
-        name: 'Geist',
-        cssVariable: '--font-geist',
+        name: "Geist",
+        cssVariable: "--font-geist",
         weights: [400, 500, 600, 700],
-        styles: ['normal'],
-        subsets: ['latin'],
+        styles: ["normal"],
+        subsets: ["latin"],
       },
       {
         provider: fontProviders.google(),
-        name: 'Geist Mono',
-        cssVariable: '--font-geist-mono',
+        name: "Geist Mono",
+        cssVariable: "--font-geist-mono",
         weights: [400, 500],
-        styles: ['normal'],
-        subsets: ['latin'],
+        styles: ["normal"],
+        subsets: ["latin"],
       },
     ],
   },
+
+  adapter: cloudflare(),
 });
